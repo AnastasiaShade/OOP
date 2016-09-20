c867ba2c4d2495d200533e87330b6d90b20f3720
Nastya Starodubtseva <anstasiashade@gmail.com>
2016-09-20 21:07:13 +0300

    Вынос части кода в функции
    Добавлены функции проверки открытия файлов и замены подстроки

--- lw1/replace/replace.cpp
+++ lw1/replace/replace.cpp
@@ -5,6 +5,48 @@
 
 using namespace std;
 
+bool isFilesOpen(char * argv[], ifstream &input, ofstream &output)
+{
+	if (!input.is_open())
+	{
+		cout << "Failed to open " << argv[1] << " for reading\n";
+		return false;
+	}
+	if (!output.is_open())
+	{
+		cout << "Failed to open " << argv[2] << " for writing\n";
+		return false;
+	}
+	return true;
+}
+
+bool isStringFind(ifstream &input, ofstream &output, string search, string replace)
+{
+	string fileLine, tempLine;
+	bool find = false;
+	while (getline(input, fileLine))
+	{
+		tempLine = "";
+		size_t pos = 0, startPos = 0;
+		while ((pos = fileLine.find(search, pos)) != string::npos)
+		{
+			tempLine += fileLine.substr(startPos, pos - startPos) + replace;
+			pos += search.length();
+			startPos = pos;
+			if ((pos != 0) && !find)
+				find = true;
+		}
+		if (!find)
+			output << fileLine << "\n";
+		else
+		{
+			tempLine += fileLine.substr(startPos, string::npos - startPos);
+			output << tempLine << "\n";
+		}
+	}
+	return find;
+}
+
 int main(int argc, char * argv[])
 {
 	if (argc != 5)
@@ -12,59 +54,25 @@ int main(int argc, char * argv[])
 		cout << "Invalid arguments count\n" << "Usage: copyfile.exe <input file> <output file> <search string> <replace string>\n";
 		return 1;
 	}
-	// Объявили переменную типа ifstream (input file stream, поток для чтения из файла), проинициализировав его именем входного файла
 	ifstream input(argv[1]);
-	// вызываем метод is_open() у объекта input, который вернет true, если файл был открыт
-	if (!input.is_open())
-	{
-		cout << "Failed to open " << argv[1] << " for reading\n";
-		return 1;
-	}
-	// создали поток для записи в выходной файл
 	ofstream output(argv[2]);
-	if (!output.is_open())
-	{
-		cout << "Failed to open " << argv[2] << " for writing\n";
+	if (!isFilesOpen(argv, input, output))
 		return 1;
-	}
-	string searchString = argv[3], replaceString = argv[4], fileString, tempString;
-	bool findStr = false;
+	string searchString = argv[3], replaceString = argv[4];
 	if (searchString.length() == 0)
 	{
 		cout << "Search string shouldn't be empty\n";
 		return 1;
 	}
-	// построчно считываем файл
-	while (getline(input, fileString))
-	{
-		//поиск подстроки в строке
-		tempString = "";
-		size_t pos = 0, startPos = 0, lenght = 0;
-		while ((pos = fileString.find(searchString, pos)) != string::npos)
-		{
-			tempString += fileString.substr(startPos, pos - startPos) + replaceString;
-			pos += searchString.length();
-			startPos = pos;
-			if ((pos != 0) && !findStr)
-				findStr = true;
-		}
-		//вывод строки в файл
-		if (!findStr)
-			output << fileString << "\n";
-		else
-		{
-			tempString += fileString.substr(startPos, string::npos - startPos);
-			output << tempString << "\n";
-		}
-	}
-	if (!output.flush()) // Если не удалось сбросить данные на диск
+	bool findStr = isStringFind(input, output, searchString, replaceString);
+	if (!output.flush())
 	{
 		cout << "Failed to save data on disk\n";
 		return 1;
 	}
 	else
 		if (!findStr)
-			cout << "The string " << searchString << " has'n been found\n";
+			cout << "The string '" << searchString << "' has'n been found\n";
 
 	return 0;
 }
