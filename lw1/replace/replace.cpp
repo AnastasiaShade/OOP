e02a609ffaed4a259b197e129629ddb96f30b60a
Nastya Starodubtseva <anstasiashade@gmail.com>
2016-09-25 23:33:05 +0300

    Изменены функции

--- lw1/replace/replace.cpp
+++ lw1/replace/replace.cpp
@@ -5,46 +5,46 @@
 
 using namespace std;
 
-bool isFilesOpen(char * argv[], ifstream &input, ofstream &output)
+bool AreFilesOpened(ifstream &input, ofstream &output, string inputFileName, string outputFileName)
 {
 	if (!input.is_open())
 	{
-		cout << "Failed to open " << argv[1] << " for reading\n";
+		cout << "Failed to open " << inputFileName << " for reading\n";
 		return false;
 	}
 	if (!output.is_open())
 	{
-		cout << "Failed to open " << argv[2] << " for writing\n";
+		cout << "Failed to open " << outputFileName << " for writing\n";
 		return false;
 	}
 	return true;
 }
 
-bool isStringFind(ifstream &input, ofstream &output, string search, string replace)
+string ReplaceSearchString(string fileLine, string searchString, string replaceString, ofstream &output)
 {
-	string fileLine, tempLine;
-	bool find = false;
+	string newString = "";
+	size_t searchPosition = 0, replacePosition = fileLine.find(searchString);
+	while (replacePosition != string::npos)
+	{
+		newString.append(fileLine, searchPosition, replacePosition - searchPosition);
+		newString += replaceString;
+		searchPosition = replacePosition + searchString.length();
+		replacePosition = fileLine.find(searchString, searchPosition);
+	}
+	output << newString.append(fileLine, searchPosition, replacePosition - searchPosition) << "\n";
+	return newString;
+}
+
+bool IsSearchStringReplaced(ifstream &input, ofstream &output, string search, string replace)
+{
+	string fileLine;
+	bool searchStringFound = false;
 	while (getline(input, fileLine))
 	{
-		tempLine = "";
-		size_t pos = 0, startPos = 0;
-		while ((pos = fileLine.find(search, pos)) != string::npos)
-		{
-			tempLine += fileLine.substr(startPos, pos - startPos) + replace;
-			pos += search.length();
-			startPos = pos;
-			if ((pos != 0) && !find)
-				find = true;
-		}
-		if (!find)
-			output << fileLine << "\n";
-		else
-		{
-			tempLine += fileLine.substr(startPos, string::npos - startPos);
-			output << tempLine << "\n";
-		}
+		if (fileLine != ReplaceSearchString(fileLine, search, replace, output))
+			searchStringFound = true;
 	}
-	return find;
+	return searchStringFound;
 }
 
 int main(int argc, char * argv[])
@@ -54,9 +54,10 @@ int main(int argc, char * argv[])
 		cout << "Invalid arguments count\n" << "Usage: copyfile.exe <input file> <output file> <search string> <replace string>\n";
 		return 1;
 	}
+
 	ifstream input(argv[1]);
 	ofstream output(argv[2]);
-	if (!isFilesOpen(argv, input, output))
+	if (!AreFilesOpened(input, output, argv[1], argv[2]))
 		return 1;
 	string searchString = argv[3], replaceString = argv[4];
 	if (searchString.length() == 0)
@@ -64,15 +65,13 @@ int main(int argc, char * argv[])
 		cout << "Search string shouldn't be empty\n";
 		return 1;
 	}
-	bool findStr = isStringFind(input, output, searchString, replaceString);
+	else
+		if (!IsSearchStringReplaced(input, output, searchString, replaceString))
+			cout << "Search string '" << searchString << "' hasn't been found.\n";
 	if (!output.flush())
 	{
 		cout << "Failed to save data on disk\n";
 		return 1;
 	}
-	else
-		if (!findStr)
-			cout << "The string '" << searchString << "' has'n been found\n";
-
 	return 0;
 }
