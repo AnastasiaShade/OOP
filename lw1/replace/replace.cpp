5c0a7912c188de051d3cf5c437a6b1008930ba44
Nastya Starodubtseva <anstasiashade@gmail.com>
2016-10-04 23:39:23 +0300

    Изменены названия переменных

--- lw1/replace/replace.cpp
+++ lw1/replace/replace.cpp
@@ -5,6 +5,42 @@
 
 using namespace std;
 
+bool AreFilesOpened(ifstream &input, ofstream &output, string inputFileName, string outputFileName);
+string Replace(string fileLine, string searchString, string replaceString, ofstream &output);
+bool IsSearchStringReplaced(ifstream &input, ofstream &output, string search, string replace);
+
+int main(int argc, char * argv[])
+{
+	if (argc != 5)
+	{
+		cout << "Invalid arguments count\n" << "Usage: copyfile.exe <input file> <output file> <search string> <replace string>\n";
+		return 1;
+	}
+
+	ifstream input(argv[1]);
+	ofstream output(argv[2]);
+
+	if (!AreFilesOpened(input, output, argv[1], argv[2]))
+		return 1;
+
+	string searchString = argv[3], replaceString = argv[4];
+	if (searchString.length() == 0)
+	{
+		cout << "Search string shouldn't be empty\n";
+		return 1;
+	}
+	else
+		if (!IsSearchStringReplaced(input, output, searchString, replaceString))
+			cout << "Search string '" << searchString << "' hasn't been found.\n";
+
+	if (!output.flush())
+	{
+		cout << "Failed to save data on disk\n";
+		return 1;
+	}
+	return 0;
+}
+
 bool AreFilesOpened(ifstream &input, ofstream &output, string inputFileName, string outputFileName)
 {
 	if (!input.is_open())
@@ -20,7 +56,7 @@ bool AreFilesOpened(ifstream &input, ofstream &output, string inputFileName, str
 	return true;
 }
 
-string ReplaceSearchString(string fileLine, string searchString, string replaceString, ofstream &output)
+string Replace(string fileLine, string searchString, string replaceString, ofstream &output)
 {
 	string newString = "";
 	size_t searchPosition = 0, replacePosition = fileLine.find(searchString);
@@ -38,40 +74,11 @@ string ReplaceSearchString(string fileLine, string searchString, string replaceS
 bool IsSearchStringReplaced(ifstream &input, ofstream &output, string search, string replace)
 {
 	string fileLine;
-	bool searchStringFound = false;
+	bool isSearchStringFound = false;
 	while (getline(input, fileLine))
 	{
-		if (fileLine != ReplaceSearchString(fileLine, search, replace, output))
-			searchStringFound = true;
+		if (fileLine != Replace(fileLine, search, replace, output))
+			isSearchStringFound = true;
 	}
-	return searchStringFound;
-}
-
-int main(int argc, char * argv[])
-{
-	if (argc != 5)
-	{
-		cout << "Invalid arguments count\n" << "Usage: copyfile.exe <input file> <output file> <search string> <replace string>\n";
-		return 1;
-	}
-
-	ifstream input(argv[1]);
-	ofstream output(argv[2]);
-	if (!AreFilesOpened(input, output, argv[1], argv[2]))
-		return 1;
-	string searchString = argv[3], replaceString = argv[4];
-	if (searchString.length() == 0)
-	{
-		cout << "Search string shouldn't be empty\n";
-		return 1;
-	}
-	else
-		if (!IsSearchStringReplaced(input, output, searchString, replaceString))
-			cout << "Search string '" << searchString << "' hasn't been found.\n";
-	if (!output.flush())
-	{
-		cout << "Failed to save data on disk\n";
-		return 1;
-	}
-	return 0;
+	return isSearchStringFound;
 }
